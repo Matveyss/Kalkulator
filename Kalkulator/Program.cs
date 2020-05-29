be952be02c593eb18f780a0868abd757cb221694
Matvey <PC@PC->
2020-05-29 20:47:05 +0300

    Добавьте файлы проекта.

---
+++ Kalkulator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalkulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число:");
+            var a = Console.ReadLine();
+            Console.WriteLine("Введите второе число:");
+            var b = Console.ReadLine();
+            Console.WriteLine("Задайте знак:\n(/ деление;* умножение;- вычитание;+ сложение)");
+            var znak = Console.ReadLine();
+            bool znak = true;
+            if znak 
+
+
+
+        }
+    }
+}
